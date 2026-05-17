f1ea9205c24ef7eb750a8f05f338d006a4807f5e
imes <shinnapat06603@varee.ac.th>
2026-05-17 18:21:36 +0700

    state logging ugh

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@ using namespace geode::prelude;
 namespace {
     constexpr char const* kEnabledSetting = "enabled";
     constexpr char const* kLoggingSetting = "logging";
+    constexpr char const* kStateLoggingSetting = "stateLogging";
     constexpr double kEpsilon = 0.000001;
     constexpr double kSlopeTolerance = 0.03;
     constexpr double kAnchorDriftTolerance = 0.125;
@@ -16,6 +17,7 @@ namespace {
 
     bool g_fixEnabled = true;
     bool g_loggingEnabled = false;
+    bool g_stateLoggingEnabled = false;
 
     bool fixEnabled() {
         return g_fixEnabled;
@@ -25,15 +27,23 @@ namespace {
         return g_loggingEnabled;
     }
 
+    bool stateLoggingEnabled() {
+        return g_stateLoggingEnabled;
+    }
+
     $on_mod(Loaded) {
         g_fixEnabled = Mod::get()->getSettingValue<bool>(kEnabledSetting);
         g_loggingEnabled = Mod::get()->getSettingValue<bool>(kLoggingSetting);
+        g_stateLoggingEnabled = Mod::get()->getSettingValue<bool>(kStateLoggingSetting);
 
         listenForSettingChanges<bool>(kEnabledSetting, [](bool value) {
             g_fixEnabled = value;
         });
         listenForSettingChanges<bool>(kLoggingSetting, [](bool value) {
             g_loggingEnabled = value;
+        });
+        listenForSettingChanges<bool>(kStateLoggingSetting, [](bool value) {
+            g_stateLoggingEnabled = value;
         });
     }
 
@@ -91,7 +101,18 @@ class $modify(WaveFixPlayerObject, PlayerObject) {
         return fixEnabled() && m_isDart && !m_isDead;
     }
 
-    void clearAnchor() {
+    void clearAnchor(char const* reason = "unspecified") {
+        if (stateLoggingEnabled() && m_fields->anchorValid) {
+            log::info(
+                "anchor cleared reason={} prevAnchor=({}, {}) ratio={} direction={} xDirection={}",
+                reason,
+                m_fields->anchorX,
+                m_fields->anchorY,
+                m_fields->ratio,
+                m_fields->direction,
+                m_fields->xDirection
+            );
+        }
         m_fields->anchorValid = false;
         m_fields->slopeStateActive = false;
         m_fields->anchorX = 0.0;
@@ -116,7 +137,16 @@ class $modify(WaveFixPlayerObject, PlayerObject) {
         );
     }
 
-    void resetWaveTrail() {
+    void resetWaveTrail(char const* reason = "unspecified") {
+        if (stateLoggingEnabled()) {
+            log::info(
+                "waveTrail reset reason={} pos=({}, {}) hasTrail={}",
+                reason,
+                m_position.x,
+                m_position.y,
+                m_waveTrail != nullptr
+            );
+        }
         if (m_waveTrail != nullptr) {
             m_waveTrail->reset();
         }
@@ -144,7 +174,19 @@ class $modify(WaveFixPlayerObject, PlayerObject) {
         return signedXTravel >= -kAnchorDriftTolerance;
     }
 
-    void seedAnchor(cocos2d::CCPoint const& position, double ratio, double direction, double xDirection) {
+    void seedAnchor(cocos2d::CCPoint const& position, double ratio, double direction, double xDirection, char const* reason = "unspecified") {
+        if (stateLoggingEnabled()) {
+            log::info(
+                "anchor seed reason={} pos=({}, {}) ratio={} direction={} xDirection={} reseed={}",
+                reason,
+                position.x,
+                position.y,
+                ratio,
+                direction,
+                xDirection,
+                m_fields->anchorValid
+            );
+        }
         m_fields->anchorValid = true;
         m_fields->anchorX = static_cast<double>(position.x);
         m_fields->anchorY = static_cast<double>(position.y);
@@ -180,7 +222,7 @@ class $modify(WaveFixPlayerObject, PlayerObject) {
 
     void setPosition(cocos2d::CCPoint const& position) {
         if (!shouldFixWave()) {
-            clearAnchor();
+            clearAnchor("shouldFixWave=false");
             PlayerObject::setPosition(position);
             return;
         }
@@ -191,6 +233,13 @@ class $modify(WaveFixPlayerObject, PlayerObject) {
         auto ratio = waveRatio(m_vehicleSize);
         auto xDirection = currentXDirection();
 
+        if (stateLoggingEnabled() && m_wasTeleported) {
+            log::info(
+                "teleport flag set current=({}, {}) requested=({}, {}) delta=({}, {})",
+                current.x, current.y, position.x, position.y, deltaX, deltaY
+            );
+        }
+
         // Ignore idle frame, probably doesn't happen in normal mode but maybe in platformer mode.
         if (isZeroDelta(deltaX, deltaY)) {
             PlayerObject::setPosition(position);
@@ -199,16 +248,31 @@ class $modify(WaveFixPlayerObject, PlayerObject) {
 
         auto currentSlopeState = isSlopeStateActive();
 
+        if (stateLoggingEnabled() && currentSlopeState != m_fields->slopeStateActive) {
+            log::info(
+                "slopeState transition active={} colliding={} onSlope={} sliding={} cur={} cur2={} potential={} pos=({}, {})",
+                currentSlopeState,
+                m_isCollidingWithSlope,
+                m_isOnSlope,
+                m_slopeSlidingMaybeRotated,
+                m_currentSlope != nullptr,
+                m_currentSlope2 != nullptr,
+                m_currentPotentialSlope != nullptr,
+                current.x, current.y
+            );
+        }
+
         if (currentSlopeState && !m_fields->slopeStateActive) {
-            resetWaveTrail();
+            resetWaveTrail("slope-enter");
         }
 
         m_fields->slopeStateActive = currentSlopeState;
 
         if (isLargeJump(deltaX, deltaY) || m_wasTeleported) {
+            char const* reason = m_wasTeleported ? "teleport" : "large-jump";
             PlayerObject::setPosition(position);
-            seedAnchor(m_position, ratio, 0.0, xDirection);
-            resetWaveTrail();
+            seedAnchor(m_position, ratio, 0.0, xDirection, reason);
+            resetWaveTrail(reason);
             return;
         }
 
@@ -221,7 +285,7 @@ class $modify(WaveFixPlayerObject, PlayerObject) {
             PlayerObject::setPosition(position);
 
             if (!m_fields->anchorValid) {
-                seedAnchor(m_position, ratio, 0.0, xDirection);
+                seedAnchor(m_position, ratio, 0.0, xDirection, "non-wave-move");
             }
 
             return;
@@ -237,7 +301,14 @@ class $modify(WaveFixPlayerObject, PlayerObject) {
             !anchorMatchesCurrent(current, ratio, xDirection) ||
             (m_fields->direction != 0.0 && m_fields->direction != direction)
         ) {
-            seedAnchor(current, ratio, direction, xDirection);
+            char const* reason =
+                !m_fields->anchorValid ? "reseed-invalid" :
+                m_fields->ratio != ratio ? "reseed-ratio" :
+                m_fields->xDirection != xDirection ? "reseed-xdir" :
+                !anchorSupportsCurrentX(current, xDirection) ? "reseed-xback" :
+                !anchorMatchesCurrent(current, ratio, xDirection) ? "reseed-drift" :
+                "reseed-flip";
+            seedAnchor(current, ratio, direction, xDirection, reason);
         } else if (m_fields->direction == 0.0) {
             m_fields->direction = direction;
         }
@@ -287,12 +358,16 @@ class $modify(WaveFixPlayerObject, PlayerObject) {
     }
 
     void toggleDartMode(bool enable, bool noEffects) {
+        if (stateLoggingEnabled()) {
+            log::info("toggleDartMode enable={} noEffects={} isDart={} isDead={}", enable, noEffects, m_isDart, m_isDead);
+        }
+
         PlayerObject::toggleDartMode(enable, noEffects);
 
-        clearAnchor();
+        clearAnchor("toggle-dart");
 
         if (enable && m_isDart && !m_isDead) {
-            seedAnchor(m_position, waveRatio(m_vehicleSize), 0.0, currentXDirection());
+            seedAnchor(m_position, waveRatio(m_vehicleSize), 0.0, currentXDirection(), "toggle-dart");
         }
     }
 };
